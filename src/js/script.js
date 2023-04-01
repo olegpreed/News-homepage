097f5f87ce9beaea9a5f97da90477bf29d298344
Oleg Preed <leo.silveri@mail.ru>
2023-04-01 18:43:05 +0700

    feat: add sass files Sat, Apr  1, 2023  6:43:04 PM

---
+++ src/js/script.js
@@ -0,0 +1,11 @@
+let num = 42;
+// const smth = document.querySelector('top-header');
+// const sideMenu = document.createElement('nav');
+// sideMenu.innerHTML = `<img src="../assets/images/icon-menu-close.svg" alt="">
+// <ul class="sidebar-menu-list">
+// 	<li><a href="">Home</a></li>
+// 	<li><a href="">New</a></li>
+// 	<li><a href="">Popular</a></li>
+// 	<li><a href="">Trending</a></li>
+// 	<li><a href="">Categories</a></li>
+// </ul>`;
